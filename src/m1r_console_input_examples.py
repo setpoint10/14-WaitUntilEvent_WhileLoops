a5fe5528006987dfaf9d66bee4d4e0b54f1d7b8e
xiny <xiny@rose-hulman.edu>
2019-01-09 10:17:54 -0500

    hmmm

--- src/m1r_console_input_examples.py
+++ src/m1r_console_input_examples.py
@@ -15,15 +15,15 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 #
 #   1. Write a line of code that would input an INTEGER from the
 #      console, storing the integer in a variable called 'x'.
-#      Write your line here:x=int(input(integar))
+#      Write your line here:x=int(input("integar"))
 #
 #   2. Write a line of code that would input an FLOAT from the console,
 #      storing the float in a variable called 'x'.
-#      Write your line here:x=float(input(a float))
+#      Write your line here:x=float(input("a float"))
 #
 #   3. Write a line of code that would input an STRING from the console,
 #      storing the string in a variable called 'x'.
-#      Write your line here:x=input(a string)
+#      Write your line here:x=input("a string")
 #
 #   4. What happens if you (the user) enter something OTHER than a
 #      single integer (e.g., you enter
--- src/m2_console_input.py
+++ src/m2_console_input.py
@@ -3,8 +3,8 @@ This module demonstrates lets you practice INPUT from the CONSOLE.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Yu Xin.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -34,6 +34,9 @@ def double_a_float():
     # TODO: 2. Implement and test this function.
     #   The testing code is already written for you (above).
     # -------------------------------------------------------------------------
+    a=int(input("enter a float"))
+    a=a*2
+    print(a)
 
 
 def print_an_integer_many_times():
